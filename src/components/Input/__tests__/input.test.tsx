67630fe709d570eab7a4c2bb5c2a86cc37a341ff
Pedro "Kermit" Gonçalves <pedro.goncalves@talkdesk.com>
2020-03-06 16:12:58 +0000

    style: 💄 fixes stylling issues

--- src/components/Input/__tests__/input.test.tsx
+++ src/components/Input/__tests__/input.test.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react'
-import { mount } from 'enzyme'
-import Input from '..'
+import * as React from 'react';
+import { mount } from 'enzyme';
+import Input from '..';
 
 describe('Input', () => {
   it('Renders Input', () => {
     const input = mount(
       <Input title="Input field" onChange={() => null} value="10" />
-    )
-    expect(input).toMatchSnapshot()
-  })
-})
+    );
+    expect(input).toMatchSnapshot();
+  });
+});
